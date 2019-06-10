e243e38c284176767bf614de684e61e4c24793e8
jstran9 <wptran58@gmail.com>
2019-06-10 11:28:54 -0700

    adding DELETE URL link.

--- frontend/gui/src/Constants.js
+++ frontend/gui/src/Constants.js
@@ -1,4 +1,5 @@
 export const BASE_API_URL = 'http://localhost:8000/api/';
 export const CREATE_API_URL = `${BASE_API_URL}create`;
 export const UPDATE_API_URL = `${BASE_API_URL}update/`;
+export const DELETE_API_URL = `${BASE_API_URL}delete/`;
 
