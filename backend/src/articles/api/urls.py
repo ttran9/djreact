bef81b6ef5a7ae49532848d71b9907cb1ee58e3b
jstran9 <wptran58@gmail.com>
2019-06-09 23:34:19 -0700

    updating gitignore with proper directory.

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDetailView, ArticleCreateView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 
 urlpatterns = [
     path('create', ArticleCreateView.as_view()),
+    path('update/<pk>', ArticleUpdateView.as_view()),
     path('<pk>', ArticleDetailView.as_view()),
     path('', ArticleListView.as_view()),
 ]
