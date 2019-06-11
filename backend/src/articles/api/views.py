90388f4c580eeea8f41ce5784313d86e0ae0a8a2
jstran9 <wptran58@gmail.com>
2019-06-10 19:28:28 -0700

    using viewset to eliminate redundant class based views.

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,32 +1,51 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework import viewsets
 
 from articles.models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleListView(ListAPIView):
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-
-class ArticleDetailView(RetrieveAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleCreateView(CreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleUpdateView(UpdateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleDeleteView(DestroyAPIView):
-    serializer_class = ArticleSerializer
-
-    def get_queryset(self):
-        queryset = Article.objects.filter(id=self.kwargs['pk'])
-        return queryset
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     UpdateAPIView,
+#     DestroyAPIView
+# )
+#
+# from articles.models import Article
+# from .serializers import ArticleSerializer
+#
+#
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleDeleteView(DestroyAPIView):
+#     serializer_class = ArticleSerializer
+#
+#     def get_queryset(self):
+#         queryset = Article.objects.filter(id=self.kwargs['pk'])
+#         return queryset
