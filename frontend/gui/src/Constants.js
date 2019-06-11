720b6d213aad9ae73711b8e896ba14e47717cc9f
jstran9 <wptran58@gmail.com>
2019-06-10 19:27:17 -0700

    removing unused URLs.

--- frontend/gui/src/Constants.js
+++ frontend/gui/src/Constants.js
@@ -1,5 +1,1 @@
-export const BASE_API_URL = 'http://localhost:8000/api/';
-export const CREATE_API_URL = `${BASE_API_URL}create`;
-export const UPDATE_API_URL = `${BASE_API_URL}update/`;
-export const DELETE_API_URL = `${BASE_API_URL}delete/`;
-
+export const BASE_API_URL = 'http://127.0.0.1:8000/api/';
