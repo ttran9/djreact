cb70f18591436cd75ef7a3c5f9fa8ab23222815e
jstran9 <wptran58@gmail.com>
2019-06-10 19:27:59 -0700

    using urlpatterns from django rest framework.

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,12 +1,24 @@
-from django.urls import path
+# from django.urls import path
+#
+# from .views import (
+#     ArticleListView,
+#     ArticleDetailView,
+#     ArticleCreateView,
+#     ArticleUpdateView,
+#     ArticleDeleteView
+# )
 
-from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
+# urlpatterns = [
+#     path('create', ArticleCreateView.as_view()),
+#     path('update/<pk>', ArticleUpdateView.as_view()),
+#     path('delete/<pk>', ArticleDeleteView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+#     path('', ArticleListView.as_view()),
+# ]
 
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('create', ArticleCreateView.as_view()),
-    path('update/<pk>', ArticleUpdateView.as_view()),
-    path('delete/<pk>', ArticleDeleteView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-    path('', ArticleListView.as_view()),
-]
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, base_name='articles')
+urlpatterns = router.urls
