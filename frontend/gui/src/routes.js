6df46d1c85a609639e8d7922e2db42a72c5eb8c7
jstran9 <wptran58@gmail.com>
2019-06-10 19:30:54 -0700

    removing unused routes.

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -3,16 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import ArticleListView from './containers/ArticleListView';
 import ArticleDetailView from './containers/ArticleDetailView';
-import ArticleCreateUpdateView from './containers/ArticleCreateUpdateView';
 
 
 const BaseRouter = () => (
     <div>
          <Switch>
-            <Route exact path='/create' component={ArticleCreateUpdateView}/>
             <Route exact path='/' component={ArticleListView}/>
             <Route exact path='/:articleID' component={ArticleDetailView}/>
-            <Route exact path='/update/:articleID' component={ArticleCreateUpdateView}/>
         </Switch>
     </div>
 );
