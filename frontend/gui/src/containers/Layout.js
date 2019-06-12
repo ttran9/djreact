c118a5d037c75d29abe04587bf2ec7253eb41ce1
jstran9 <wptran58@gmail.com>
2019-06-11 17:23:45 -0700

    integrating redux and displaying the appearance of our navigation bar.

--- frontend/gui/src/containers/Layout.js
+++ frontend/gui/src/containers/Layout.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import * as actions from "../store/actions/auth";
+import { connect } from "react-redux";
 const { Header, Content, Footer } = Layout;
 
-const CustomLayout = (props) => {
-    return (
+class CustomLayout extends Component {
+
+    render() {
+         return (
       <Layout className="layout">
         <Header>
           <div className="logo" />
@@ -14,9 +18,28 @@ const CustomLayout = (props) => {
             defaultSelectedKeys={['2']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+          {
+            this.props.isAuthenticated ?
+
+            <Menu.Item key="2" onClick={this.props.logout}>
+                Logout
+            </Menu.Item>
+
+            :
+
+            <Menu.Item key="2">
+                <Link to="/login/">
+                    Login
+                </Link>
+            </Menu.Item>
+          }
+
+              <Menu.Item key="1">
+                <Link to="/">
+                    Posts
+                </Link>
+            </Menu.Item>
+
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
@@ -25,12 +48,19 @@ const CustomLayout = (props) => {
               <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
           </Breadcrumb>
           <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-              {props.children}
+              {this.props.children}
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
     );
+    }
 };
 
-export default CustomLayout;
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: () => dispatch(actions.logout())
+    }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(CustomLayout));
