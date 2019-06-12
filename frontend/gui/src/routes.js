5cbc90674d3957812ca62a9a279694595c983100
jstran9 <wptran58@gmail.com>
2019-06-11 17:26:41 -0700

    adding in routes for the login and signup and refactoring the single article route.

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -3,14 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 
 import ArticleListView from './containers/ArticleListView';
 import ArticleDetailView from './containers/ArticleDetailView';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 
 const BaseRouter = () => (
     <div>
          <Switch>
-            <Route exact path='/' component={ArticleListView}/>
-            <Route exact path='/:articleID' component={ArticleDetailView}/>
-        </Switch>
+             <Route exact path='/' component={ArticleListView}/>
+             <Route exact path='/articles/:articleID' component={ArticleDetailView}/>
+             <Route exact path='/login/' component={Login}/>
+             <Route exact path='/signup/' component={Signup}/>
+         </Switch>
     </div>
 );
 
