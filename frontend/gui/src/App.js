2fe615076bf0bf79cc950cec072ef072e261bd48
jstran9 <wptran58@gmail.com>
2019-06-11 17:21:11 -0700

    connecting redux to our application.

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,21 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
+import { connect } from 'react-redux';
 import BaseRouter from './routes';
 import 'antd/dist/antd.css';
+import * as actions from './store/actions/auth';
 
 import CustomLayout from './containers/Layout';
 
-function App() {
+class App extends Component {
+
+    componentDidMount() {
+        this.props.onTryAutoSignup();
+    }
+
+    /*
+     * connect can grab the store that we connected back in index.js
+     * this allows us to access some of the state in the store.
+     * every time our app is rendered our app checks if we are authenticated and this is where
+     * mapStateToProps comes in.
+     */
     /* BaseRouter evaluates what component needs to be displayed there */
-    return (
-      <div className="App">
-        <Router>
-            <CustomLayout>
-            <BaseRouter />
-          </CustomLayout>
-        </Router>
-    </div>
-  );
+    /* doing ...this.props will allow access to it inside of our customlayout component as well. */
+    render() {
+        return (
+        <div>
+            <Router>
+                <CustomLayout {...this.props}>
+                    <BaseRouter />
+                </CustomLayout>
+            </Router>
+        </div>
+        );
+    }
+
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        /*
+         * in this object is what we want to map into a property.
+         * this property is passed into the connect method.
+         */
+        isAuthenticated: state.token !== null, // if our token is null isAuthenticated is false.
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState())
+    }
+}
+
+
+/*
+ * inside of connect method/function we can pass in two functions
+ * the first function is mapStatesToProps
+ * convert states (from the store (from react-redux)) into properties that we can pass into our application.
+ * the second is mapDispatchToProps
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(App);
