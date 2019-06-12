7a2068e002ced853ad979a06d912751e68f8074b
jstran9 <wptran58@gmail.com>
2019-06-11 17:20:56 -0700

    adding in additional urls.

--- frontend/gui/src/Constants.js
+++ frontend/gui/src/Constants.js
@@ -1,1 +1,4 @@
 export const BASE_API_URL = 'http://127.0.0.1:8000/api/';
+export const REST_AUTH_URL = 'http://127.0.0.1:8000/rest-auth/'
+export const LOGIN_URL = `${REST_AUTH_URL}login/`;
+export const SIGNUP_URL = `${REST_AUTH_URL}registration/`;
