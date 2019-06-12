f9db501203c1c4417fd5ee6da552308caea4a1d9
jstran9 <wptran58@gmail.com>
2019-06-11 17:18:46 -0700

    adding necessary mappings to be able to process login and registration requests.

--- backend/src/src/urls.py
+++ backend/src/src/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('admin/', admin.site.urls),
     path('api/', include('articles.api.urls')),
 ]
 
