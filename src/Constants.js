1f7e324f9219b7e27b98e1790cf636f2c1cca81b
jstran9 <wptran58@gmail.com>
2019-06-13 17:17:04 -0700

    modifying BASE_URL to read from an environment variable.

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://djreact-todd.herokuapp.com/';
+export const BASE_URL = process.env.BASE_URL;
 export const BASE_API_URL = `${BASE_URL}api/`;
 export const REST_AUTH_URL = `${BASE_URL}rest-auth/`;
 export const LOGIN_URL = `${REST_AUTH_URL}login/`;
